dc293802273de75a30f42014b1afded1cddbd6b8
artday <admin@artday.com.ua>
2018-07-09 17:16:32 +0300

    refactor service API

--- src/app/life/life.component.ts
+++ src/app/life/life.component.ts
@@ -4,66 +4,36 @@ import {GenerationService} from '../generation.service';
 @Component({
   selector: 'app-life',
   templateUrl: './life.component.html',
-  styleUrls: ['./life.component.css']
+  styleUrls: ['./life.component.css'],
 })
 export class LifeComponent implements OnInit {
 
   genCnt: number;
   lifeSize: number = 50;
-  generation: Boolean[][];
+  generation: boolean[][];
   randomRange: number = -0.7;
   speed = -300;
 
   isClear: boolean;
   isPlay: boolean;
+  drawed: number[][] = [];
 
-  private interval;
-  nextGeneration = [];
+  private interval: number;
 
-  constructor(private service: GenerationService) {
+  constructor(private service: GenerationService) {}
+
+  /* add cells changed manually to drawArray for saving figure; */
+  draw(pos: number[]) {
+    this.isClear = false;
+    this.drawed.push(pos);
   }
 
   toggle(pos: number[]) {
-    /* TODO: add cells changed manually to drawArray for saving figure; */
     this.generation[pos[0]][pos[1]] = !this.generation[pos[0]][pos[1]];
-    this.neighborsPos(pos);
   }
 
-  next(): void {
-    this.generation
-      .forEach((row, y) => row
-        .forEach((col, x) => {
-          const isAlive = col;
-          const aliveNeighbors = this.neighborsPos([y, x]).filter(n => this.generation[n[0]][n[1]]).length;
-          if ((aliveNeighbors > 3 || aliveNeighbors < 2) && isAlive) {
-            this.nextGeneration.push([y, x]);
-          }
-          if (!isAlive && aliveNeighbors === 3) {
-            this.nextGeneration.push([y, x]);
-          }
-        }));
-    if (!this.nextGeneration.length) {
-      this.stop();
-      return;
-    }
-    this.nextGeneration.forEach(pos => this.toggle(pos));
-    this.nextGeneration = [];
-  }
-
-  neighborsPos(pos: number[]) {
-    const y = pos[0];
-    const x = pos[1];
-    const neighbors = [];
-    for (let Y = y - 1; Y <= y + 1; Y++) {
-      for (let X = x - 1; X <= x + 1; X++) {
-        const posY = Y < 0 ? (this.lifeSize - 1) : (Y > (this.lifeSize - 1) ? 0 : Y);
-        const posX = X < 0 ? (this.lifeSize - 1) : (X > (this.lifeSize - 1) ? 0 : X);
-        if (!(posY === y && posX === x)) {
-          neighbors.push([posY, posX]);
-        }
-      }
-    }
-    return neighbors;
+  next() {
+    this.generateNext();
   }
 
   random() {
@@ -71,6 +41,7 @@ export class LifeComponent implements OnInit {
   }
 
   clear(): void {
+    this.drawed = [];
     this.generateEmpty();
   }
 
@@ -78,60 +49,61 @@ export class LifeComponent implements OnInit {
     this.stop();
     this.isPlay = true;
     this.interval = setInterval(() => this.next(), this.speed * (-1));
-    console.log(this.speed * (-1));
   }
 
   stop(): void {
-    clearInterval(this.interval);
     this.isPlay = false;
-  }
-
-
-
-  figure() {
-    for (let y = 0; y < this.lifeSize; y++) {
-      this.generation[y] = [];
-      for (let x = 0; x < this.lifeSize; x++) {
-        if (x === y || ( x + y === this.lifeSize - 1)) {
-          this.generation[y][x] = true;
-        } else {
-          this.generation[y][x] = false;
-        }
-      }
-    }
-  }
-
-  private generateEmpty(): void {
-    this.setGeneration(this.emptyGeneration());
+    clearInterval(this.interval);
   }
 
   private generateRandom(): void {
     this.setGeneration(this.randomGeneration());
   }
 
-  private setGeneration(serviceGeneration): void {
+  private generateEmpty(): void {
+    this.setGeneration(this.emptyGeneration());
+  }
+
+  private generateNext(): void {
+    this.updateGeneration(this.nextGeneration());
+  }
+
+  private updateGeneration(serviceGeneration: number[][]): void {
+    if (!serviceGeneration.length) {
+      this.isClear = false;
+      this.stop();
+      return;
+    }
+    serviceGeneration.forEach(pos => this.toggle(pos));
+  }
+
+  private setGeneration(serviceGeneration: boolean[][]): void {
     this.stop();
     this.generation = serviceGeneration;
   }
 
-  private randomGeneration(): Boolean[][] {
+  private randomGeneration(): boolean[][] {
     this.isClear = false;
     return this.service.randomGeneration(this.lifeSize, this.randomRange * (-1));
   }
 
-  private emptyGeneration(): Boolean[][] {
+  private emptyGeneration(): boolean[][] {
     this.isClear = true;
     return this.service.emptyGeneration(this.lifeSize);
   }
 
-  ngOnInit() {
+  private nextGeneration(): number[][] {
+    return this.service.mustUpdateByLiveConditions(this.generation);
+  }
+
+  ngOnInit(): void {
     this.setGeneration(this.emptyGeneration());
     this.genCnt = 1;
   }
 
-  show (){
+  /*show() {
     console.log(this.randomRange * (-1));
-  }
+  }*/
 
   /*generate(random = false) {
   /!*
@@ -170,4 +142,61 @@ export class LifeComponent implements OnInit {
       callable(i);
     }
   }*/
+
+  /*
+  neighborsPos(pos: number[]) {
+    const y = pos[0];
+    const x = pos[1];
+    const neighbors = [];
+    for (let Y = y - 1; Y <= y + 1; Y++) {
+      for (let X = x - 1; X <= x + 1; X++) {
+        const posY = Y < 0 ? (this.lifeSize - 1) : (Y > (this.lifeSize - 1) ? 0 : Y);
+        const posX = X < 0 ? (this.lifeSize - 1) : (X > (this.lifeSize - 1) ? 0 : X);
+        if (!(posY === y && posX === x)) {
+          neighbors.push([posY, posX]);
+        }
+      }
+    }
+    return neighbors;
+  }
+  */
+
+  /*
+  figure() {
+    for (let y = 0; y < this.lifeSize; y++) {
+      this.generation[y] = [];
+      for (let x = 0; x < this.lifeSize; x++) {
+        if (x === y || ( x + y === this.lifeSize - 1)) {
+          this.generation[y][x] = true;
+        } else {
+          this.generation[y][x] = false;
+        }
+      }
+    }
+  }
+  */
+
+  /*
+  next(): void {
+    this.generation
+      .forEach((row, y) => row
+        .forEach((col, x) => {
+          const isAlive = col;
+          const aliveNeighbors = this.neighborsPos([y, x]).filter(n => this.generation[n[0]][n[1]]).length;
+          if ((aliveNeighbors > 3 || aliveNeighbors < 2) && isAlive) {
+            this.nextGeneration.push([y, x]);
+          }
+          if (!isAlive && aliveNeighbors === 3) {
+            this.nextGeneration.push([y, x]);
+          }
+        }));
+    if (!this.nextGeneration.length) {
+      this.isClear = false;
+      this.stop();
+      return;
+    }
+    this.nextGeneration.forEach(pos => this.toggle(pos));
+    this.nextGeneration = [];
+  }
+  */
 }
